ce496f410be709dee148ccd7771def134563471d
Emerson Warhman <edwarhman@gmail.com>
2024-03-21 03:55:53 -0400

    fix not showing video

--- src/onboarding/index.js
+++ src/onboarding/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 
 const AppOnboarding = () => {
 
-    const [videoURL, setVideoUrl] = useState("");
+    const [videoURL, setVideoUrl] = useState();
 
     const getVideo = () => {
         fetch('http://18.169.192.176/api/config/onboarding-video/', {
@@ -20,10 +20,12 @@ const AppOnboarding = () => {
 
     return (
         <div>
+            {videoURL ? 
             <video width="320" height="240" controls>
                 <source src={videoURL} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
+            : null }
         </div>
     )
 }
