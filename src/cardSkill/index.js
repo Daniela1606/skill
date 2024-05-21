b17c51823166fca8ec7ab1b2d02ba6e8ce4fb665
Daniela1606 <alexandra16.mg@gmail.com>
2024-05-21 01:43:57 -0400

    skills added

--- src/cardSkill/index.js
+++ src/cardSkill/index.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { PlusOutlined } from '@ant-design/icons';
 import { Button, Card, Row, Col } from 'antd';
 import { imagenDeIcon, imagenDeGit, imagenDeAtom, imagenDeJs, imagenDePhy, imagenDeC, imagenDeMy, imagenAws, imagenSsl, imagenApache, imagenDeResearch, imagenDeWordpress } from '../constante/imagen';
 
 const { Meta } = Card;
 
-const CardArray = ({ searchData }) => {
-console.log('lo paso', searchData)
-    const cardData = [
+const CardArray = ({ searchData, onCardAdded }) => {
+  console.log('lo paso', searchData);
+
+  const cardData = [
     { title: 'GitHub ', image: imagenDeGit.IMAGENICON },
     { title: '', image: imagenDeAtom.IMAGENICON },
     { title: 'JavaScript ', image: imagenDeJs.IMAGENICON },
@@ -20,22 +21,18 @@ console.log('lo paso', searchData)
     { title: 'SSL', image: imagenSsl.IMAGENICON },
     { title: 'Wordpress ', image: imagenDeWordpress.IMAGENICON },
     { title: 'Research ', image: imagenDeResearch.IMAGENICON },
-    ];
-
+  ];
 
   const [filteredCards, setFilteredCards] = useState(searchData || cardData);
 
-  useEffect(() => {
-    setFilteredCards(searchData || cardData );
-  }, [searchData]);
-
-  const upperCards = filteredCards.slice(0, 6);
-  const lowerCards = filteredCards.slice(6, 12);
+  const handleAddCard = (card) => {
+    onCardAdded(card);
+  };
 
   return (
     <div>
       <Row gutter={[16, 16]}>
-        {upperCards.map((card, index) => (
+        {filteredCards.map((card, index) => (
           <Col key={index} span={4}>
             <Card
               style={{ width: '95%', border: 'none', boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px' }}
@@ -52,35 +49,8 @@ console.log('lo paso', searchData)
                   icon={<PlusOutlined />}
                   style={{ float: 'right', marginRight: '2px', background: '#00007c' }}
                   size="small"
-                >
-                </Button>
-              ]}
-            >
-              <Meta />
-            </Card>
-          </Col>
-        ))}
-      </Row>
-      <Row gutter={[16, 16]}>
-        {lowerCards.map((card, index) => (
-          <Col key={index} span={4}>
-            <Card
-              style={{ width: '95%', border: 'none', boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px' }}
-              cover={
-                <div style={{ textAlign: 'center' }}>
-                  <img style={{ width: '35px', marginTop: '0.5rem' }} src={card.image} alt="Logo" />
-                  <div style={{ color: 'black', fontWeight: '700' }}>{card.title}</div>
-                </div>
-              }
-              actions={[
-                <Button
-                  key="add"
-                  type="primary"
-                  icon={<PlusOutlined />}
-                  style={{ float: 'right', marginRight: '2px', background: '#00007c' }}
-                  size="small"
-                >
-                </Button>
+                  onClick={() => handleAddCard(card)}
+                ></Button>,
               ]}
             >
               <Meta />
--- src/cardSkillAdd/index.js
+++ src/cardSkillAdd/index.js
@@ -1,11 +1,83 @@
+
+
 import React, { useState } from 'react';
 import { PlusOutlined } from '@ant-design/icons';
 import { Button, Card, Row, Col } from 'antd';
-import { imagenDeIcon } from '../constante/imagen';
+import { imagenDeIcon, imagenDeGit, imagenDeAtom, imagenDeJs, imagenDePhy, imagenDeC, imagenDeMy, imagenAws, imagenSsl, imagenApache, imagenDeResearch, imagenDeWordpress } from '../constante/imagen';
+import CardArray from '../cardSkill';
 
 const { Meta } = Card;
 
 const CardArrayAdd = () => {
+  const [selectedCard, setSelectedCard] = useState(null);
+  const [cards, setCards] = useState([]);
+
+  const handleCardSelect = (card) => {
+    setSelectedCard(card);
+  };
+
+  const handleCardAdded = (card) => {
+    setCards([...cards, card]);
+  };
+
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        left: '-7rem',
+        width: '400px',
+      }}
+    >
+      <Row gutter={[16, 16]}>
+        {cards.map((card, index) => (
+          <Col key={index} span={8}>
+            <Card
+              style={{
+                padding: '0.5rem 1rem',
+                width: '95%',
+                border: '2px solid blue',
+                boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px',
+              }}
+              cover={
+                <div style={{ textAlign: 'center' }}>
+                  <img
+                    style={{ width: '45%', marginTop: '0.5rem' }}
+                    src={card.image}
+                    alt="Logo"
+                  />
+                  <div style={{ color: 'black', fontWeight: '700' }}>
+                    {card.title}
+                  </div>
+                </div>
+              }
+            >
+              <Meta />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+      <CardArray searchData={cards} onCardAdded={handleCardAdded} />
+    </div>
+  );
+};
+
+export default CardArrayAdd;
+
+
+
+
+
+/* import React, { useState } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
+import { Button, Card, Row, Col } from 'antd';
+import { imagenDeIcon } from '../constante/imagen';
+import CardArray from '../cardSkill';
+
+const { Meta } = Card;
+
+const CardArrayAdd = () => {
+  const [selectedCard, setSelectedCard] = useState(null);
+
   const cardData = [
     { title: 'Skill ', image: imagenDeIcon.IMAGENICON },
     { title: 'Skill ', image: imagenDeIcon.IMAGENICON },
@@ -24,7 +96,9 @@ const CardArrayAdd = () => {
   const upperCards = cardData.slice(0, 3);
   const lowerCards = cardData.slice(3, 6);
 
-  const [selectedCard, setSelectedCard] = useState(null);
+  const handleCardSelect = (card) => {
+    setSelectedCard(card);
+  };
 
   return (
     <div
@@ -57,7 +131,14 @@ const CardArrayAdd = () => {
                 </div>
               }
               actions={[
-
+                <Button
+                  key="add"
+                  type="primary"
+                  icon={<PlusOutlined />}
+                  style={{ float: 'right', marginRight: '2px', background: '#00007c' }}
+                  size="small"
+                  onClick={() => handleCardSelect(card)}
+                ></Button>
               ]}
             >
               <Meta />
@@ -88,7 +169,14 @@ const CardArrayAdd = () => {
                 </div>
               }
               actions={[
-
+                <Button
+                  key="add"
+                  type="primary"
+                  icon={<PlusOutlined />}
+                  style={{ float: 'right', marginRight: '2px', background: '#00007c' }}
+                  size="small"
+                  onClick={() => handleCardSelect(card)}
+                ></Button>
               ]}
             >
               <Meta />
@@ -100,4 +188,4 @@ const CardArrayAdd = () => {
   );
 };
 
-export default CardArrayAdd;
+export default CardArrayAdd; */
